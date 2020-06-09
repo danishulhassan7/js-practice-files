60aacfaaff3f82746829d7ace3bc4dd62b771be0
danihulhassan7 <danishulhassan7@gmail.com>
2020-06-09 18:25:20 +0500

    Ch-01 alerts

---
+++ ch-01/app1.js
@@ -0,0 +1,7 @@
+alert("Hello, I hope you are doing good");
+alert("Error: Please enter correct credentials");
+
+alert("Welcome to JS Land" + "\r\n" + "Happy Coding :) ");
+
+alert("Welcome to Js land");
+alert("Happy Coding!");
